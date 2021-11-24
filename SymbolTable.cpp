510df8f2e4fd535e3cc6a3dca90c1e4d6182383a
leoPkm <thinhphatmai2001@gmail.com>
2021-11-24 12:57:30 +0700

    read file template

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -2,17 +2,17 @@
 
 void SymbolTable::run(string filename)
 {
-    cout << "success"<<endl;
-    fstream myFile;
-    myFile.open(filename,ios::in);  //read mode
-    if(myFile.is_open()){   // kiểm tra xem file mở thanh công chưa
-        string line;
-        while (getline(myFile,line))
-        {
-            cout<<line<<endl;
+    //cout << "success"<<endl;
+    fstream myFile;                 //biến này là biến file
+    myFile.open(filename,ios::in);  //mở file ở read mode
+    if(myFile.is_open()){           // kiểm tra xem file mở thanh công chưa hàm is_open() == true nếu thành công và false nếu ngược lại
+        string line;                // biến này để chứa từng dòng lệnh trong file đọc dc 
+        while (getline(myFile,line))        //dùng vòng lặp để đọc file sau đó dòng đọc dc sẽ dc lưu trong biến line 
+        {                                   
+            cout<<line<<endl;               // xuất dòng đọc dc đang dc lưu trong biến line ra màn hình
         }
 
-        myFile.close();
+        myFile.close();                     // nhớ đóng file sau khi đọc xong
         
     }
 }
